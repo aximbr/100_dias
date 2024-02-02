13b3a0ba36acb3a6a1af00655ebaf311ac164ed3
Jose Leitao <jose.p.leitao@gmail.com>
2024-02-01 22:11:45 -0300

    Day 001

---
+++ 001/main.py
@@ -0,0 +1,4 @@
+print("Welcome the Band Name Generator 1.0!\n")
+city_name = input("What is the name of city? \n")
+pet_name = input("What is your pet's name? \n")
+print("Your Band name could be " + pet_name + " "+ city_name)
