548c9e35f17e6a2763af898e9ef51f47a5aa7643
Jose Leitao <jose.p.leitao@gmail.com>
2024-02-03 16:46:45 -0300

    Lesson 07

--- 028/main.py
+++ 028/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,20 +9,67 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
+marks = ""
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def reset_timer():
+    global reps
+    reps = 0
+    window.after_cancel(timer)
+    title_label.config(text="Timer", fg=GREEN)
+    canvas.itemconfig(timer_text, text="00:00")
+    label_checks.config(text = "" )
+    
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(WORK_MIN * 60)
+    global reps
+    reps += 1
+    
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    
+    if reps %8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+    elif reps %2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
+        
+    
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    count_min = math.floor(count/60)
+    global reps
+    global timer
+    global marks
+    
+    count_min = count//60
+    if count_min <10:
+        count_min = f"0{count_min}"
     count_sec = count%60
-    count_text = str(count_min).rjust(2,"0") + ":" + str(count_sec).rjust(2,"0")
+    if count_sec <10:
+        count_sec = f"0{count_sec}"
+    count_text = f"{count_min}:{count_sec}"
     canvas.itemconfig(timer_text, text=count_text)
+    
     if count >0:
-        window.after(1000, count_down, count-1)
+        timer = window.after(10, count_down, count-1)
+    else:
+        start_timer()
+        if reps %2 == 0:
+            marks += "✓"
+            label_checks.config(text = marks )    
+    
+    
+        
+    
+    
+    
         
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -42,13 +89,13 @@ canvas.grid(column=1, row=1)
 button_start = Button(text="Start", highlightthickness=0, command=start_timer )
 button_start.grid(column=0, row=2)
 
-button_reset = Button(text="Reset", highlightthickness=0, command="" )
+button_reset = Button(text="Reset", highlightthickness=0, command=reset_timer )
 button_reset.grid(column=2, row=2)
 
-label_checks = Label(text="✓", fg=GREEN, bg= YELLOW, font=(FONT_NAME, 12, "bold"))
+label_checks = Label(text="", fg=GREEN, bg= YELLOW, font=(FONT_NAME, 12, "bold"))
 label_checks.grid(column=1, row=3)
 
-
+#"✓"
 
 
 
