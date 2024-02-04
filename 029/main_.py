ef021a5f55886d1b3b3a5f846696bfc891a0b0f4
Jose Leitao <jose.p.leitao@gmail.com>
2024-02-04 10:42:59 -0300

    Lesson 07

--- 029/main_.py
+++ 029/main_.py
@@ -1,19 +1,66 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
 
 WHITE = "#ffffff"
 FILENAME = "data.txt"
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def gen_password():
+    entry_password.delete(0, END)
+    
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    # for char in range(nr_letters):
+    #     password_list.append(random.choice(letters))
+    #using List comprehension
+    password_list = [random.choice(letters) for _ in range(nr_letters)]
+
+    # for char in range(nr_symbols):
+    #     password_list += random.choice(symbols)
+    #using List comprehension
+    password_list += [random.choice(symbols) for _ in range(nr_symbols)]
+    # for char in range(nr_numbers):
+    #     password_list += random.choice(numbers)
+    #using List comprehension
+    password_list += [random.choice(numbers) for _ in range(nr_numbers)]
+    random.shuffle(password_list)
+
+    #using join to converter from list to a string
+    password = "".join(password_list)
+
+    entry_password.insert(0, password)
+    pyperclip.copy(password)
+    
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_to_file():
-    line = f"{entry_website.get()}|{entry_username.get()}|{entry_password.get()}'\n'"
-    with open(FILENAME, "a") as fp:
-        fp.write(line)
-    entry_website.delete(0, END)
-    entry_password.delete(0, END)
-    entry_website.focus()
+    website = entry_website.get()
+    username = entry_username.get()
+    password = entry_password.get()
+    
+    line = f"{website}|{username}|{password}\n"
+    
+    if len(website) == 0 or len(username) == 0 or len(password) == 0:
+        messagebox.showerror(title= "Error", message="Please don't leave any field empty!")
+    else:       
+        is_ok = messagebox.askokcancel(title=website, message = f"These are the details entered: \nEmail: {username}\n"
+                                f"Password: {password}\nIs Ok to save?")
+        if is_ok:       
+            with open(FILENAME, "a") as fp:
+                fp.write(line)
+            entry_website.delete(0, END)
+            entry_password.delete(0, END)
+            entry_website.focus()
         
     
      
@@ -51,7 +98,7 @@ entry_username.insert(0, "jose.p.leitao@gmail.com")
 entry_password = Entry(width=21)
 entry_password.grid(column=1, row=3, sticky="W")
 
-button_generate = Button(text="Generate Password", command=None)
+button_generate = Button(text="Generate Password", command=gen_password)
 button_generate.grid(column=2, row=3)
 
 button_add = Button(text="Add", width=36, command=save_to_file)
