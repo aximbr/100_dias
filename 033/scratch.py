718ca9c5a070dc035655959b2a38782dad9ef3d7
Jose Leitao <jose.p.leitao@gmail.com>
2024-02-08 18:58:00 -0300

    Lesson 06

--- 033/scratch.py
+++ 033/scratch.py
@@ -1,15 +1,47 @@
 import requests
 
-URL="http://api.open-notify.org/iss-now.json"
+# URL="http://api.open-notify.org/iss-now.json"
 
-response = requests.get(url=URL)
+# response = requests.get(url=URL)
+# response.raise_for_status()
+
+# data = response.json()["iss_position"]
+
+# longitude = float(data["longitude"])
+# latitude = float(data["latitude"])
+
+# iss_postion = (latitude, longitude)
+
+# print(iss_postion)
+import datetime as dt 
+
+MY_LAT=-22.097909
+MY_LONG=-51.398390
+
+URL = "https://api.sunrise-sunset.org/json"
+
+now = dt.datetime.now()
+hour_now = now.hour
+print(hour_now)
+
+parameters = {
+    "lat" : MY_LAT,
+    "lng" : MY_LONG,
+    "formatted" : 0,
+}
+
+response = requests.get(url=URL, params=parameters)
 response.raise_for_status()
 
-data = response.json()["iss_position"]
+data = response.json()
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
 
-longitude = float(data["longitude"])
-latitude = float(data["latitude"])
+sunrise = sunrise.split("T")
+sunset = sunset.split("T")
 
-iss_postion = (latitude, longitude)
+sunrise_hour = sunrise[1][0:2]
+sunset_hour = sunset[1][0:2]
 
-print(iss_postion)
+print(sunrise_hour)
+print(sunset_hour)
