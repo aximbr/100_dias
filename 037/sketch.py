ee910136380327314f0b94ff14bd3a0b26ed4d85
Jose Leitao <jose.p.leitao@gmail.com>
2024-02-11 12:07:06 -0300

    Lesson 03

--- 037/sketch.py
+++ 037/sketch.py
@@ -1,14 +1,36 @@
 import requests
 
-URL = "https://pixe.la/v1/users"
+TOKEN = "python_user"
+USERNAME = "yabadabadu"
+PIXELA = "https://pixe.la/v1/users"
 
-users_param = {
-    "token" : "python_user",
-    "username" : "yabadabadu",
-    "agreeTermsOfService" : "yes",
-    "notMinor" : "yes",
-        }
+# users_param = {
+#     "token" : TOKEN,
+#     "username" : USERNAME,
+#     "agreeTermsOfService" : "yes",
+#     "notMinor" : "yes",
+#         }
 
-response = requests.post(url=URL, json=users_param)
+# response = requests.post(url=PIXELA, json=users_param)
+
+# print(response.text)
+
+graph_endpoint = f"{PIXELA}/{USERNAME}/graphs"
+
+graph_params = {
+    "id" : "graph1",
+    "name" : "My Python Study Hours",
+    "unit" : "hrs",
+    "type" : "float",
+    "color": "sora"
+}
+
+headers = {
+    "X-USER-TOKEN" : TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
 
 print(response.text)
+
+#to test use: https://pixe.la/v1/users/yabadabadu/graphs/graph1.html
