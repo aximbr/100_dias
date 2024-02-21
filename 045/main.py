18c8616fcd9e0a0395b0bf46208ac158a04791a4
Jose Leitao <jose.p.leitao@gmail.com>
2024-02-20 21:07:22 -0300

    Lesson 03

--- 045/main.py
+++ 045/main.py
@@ -7,8 +7,14 @@ with open("website.html", "r", encoding='utf-8') as fp:
     
 soup = BeautifulSoup(contents, 'html.parser')
 
-print(soup.title)
-print(soup.title.name)
-print(soup.title.string)
-print(soup.p)
-  
+all_anchor_tags = soup.find_all(name="a")
+
+for tag in all_anchor_tags:
+    print(tag.get("href"))
+    
+heading = soup.find(name="h1", id="name")
+print(heading.text)
+
+company_url = soup.select_one("p a")
+print(company_url)
+    
