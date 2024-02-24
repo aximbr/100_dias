46c87640f24aa9f87ff5550dbf6e24ba326553e3
Jose Leitao <jose.p.leitao@gmail.com>
2024-02-24 07:54:56 -0300

    Lesson 03

--- 048/main.py
+++ 048/main.py
@@ -1,8 +1,16 @@
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+URL="https://www.amazon.com.br/LG-32LQ620-Bluetooth-ThinQAI-compat%C3%ADvel/dp/B0B5HJYL3B/ref=sr_1_5?crid=3RW4SN431E48W"
 
 driver = webdriver.Chrome()
 
-driver.get("https://www.amazon.com.br")
+driver.get(URL)
 
+price_int_txt = driver.find_element(By.CLASS_NAME,'a-price-whole').text
+price_int = int(price_int_txt.replace('.',""))
+price_frac = float(driver.find_element(By.CLASS_NAME,'a-price-fraction').text)/100
+
+print(price_int+price_frac)
 driver.quit()
