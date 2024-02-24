0afd86946ea126c13b4ffe05ac2f117f06a2f34e
Jose Leitao <jose.p.leitao@gmail.com>
2024-02-24 15:54:16 -0300

    Lesson 04

--- 048/sketch.py
+++ 048/sketch.py
@@ -13,7 +13,21 @@ driver.get(URL)
 # logo = driver.find_element(By.CLASS_NAME, 'python-logo')
 # print(logo.size)
 
-about = driver.find_element(By.XPATH,'//*[@id="container"]/li[1]/a')
-print(about.text)
+# about = driver.find_element(By.XPATH,'//*[@id="container"]/li[1]/a')
+# print(about.text)
+
+event_dates = driver.find_elements(By.CSS_SELECTOR,'.event-widget time')
+event_names = driver.find_elements(By.CSS_SELECTOR,'.event-widget li a')
+
+events = {}
+
+for n in range(len(event_dates)):
+    events[n] = {
+        "date" : event_dates[n].text,
+        "name" : event_names[n].text
+    }
+    
+
+print(events)
 
 driver.quit()
