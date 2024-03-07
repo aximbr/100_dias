4addfd27e6bf81e2a817fa4c0f43bc041bc48c89
Jose Leitao <jose.p.leitao@gmail.com>
2024-03-07 19:52:04 -0300

    Lesson 04 final

--- 056/server.py
+++ 056/server.py
@@ -11,5 +11,9 @@ def home():
 def get_index():
     return render_template("index.html" )
 
+@app.route('/single.html')
+def get_single():
+    return render_template("single.html" )
+
 if __name__ == "__main__":
     app.run(debug=True)
