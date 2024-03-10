71c2dc69adbd7476ae7747f27c96497bc6cccca5
Jose Leitao <jose.p.leitao@gmail.com>
2024-03-10 08:55:46 -0300

    Lesson 06

--- 057/main.py
+++ 057/main.py
@@ -1,48 +1,28 @@
 from flask import Flask, render_template
-import datetime
+from post import Post
 import requests
 
-URL_AGE = "https://api.agify.io?name="
-URL_GENDER = "https://api.genderize.io?name="
+all_posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+post_objects = []
 
-app = Flask(__name__)
+for post in all_posts:
+    new_post = Post(post['id'], post['title'], post['subtitle'], post['body'])
+    post_objects.append(new_post)
 
-current_year = datetime.datetime.now().year
+app = Flask(__name__)    
 
-def get_age(name):
-    response = requests.get(URL_AGE+name)
-    response.raise_for_status()
-
-    if response.status_code == 200:
-        return response.json()['age']
-    
-def get_gender(name):
-    response = requests.get(URL_GENDER+name)
-    response.raise_for_status()
-
-    if response.status_code == 200:
-        return response.json()['gender'] 
-       
-        
 @app.route('/')
 def home():
-    return render_template("index.html", year = current_year)
+    return render_template("index.html", posts=post_objects)
+  
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
-@app.route('/guess/<name_in>')
-def great(name_in):
-    name = str(name_in).title()
-    age = get_age(name)
-    gender = get_gender(name)
-    return render_template("guess.html", out_name = name, out_age = age, out_gender = gender, year = current_year, )
-
-@app.route('/blog')
-def blog():
-    blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
-    response = requests.get(blog_url)
-    all_posts = response.json()
-    return render_template('blog.html', posts=all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
